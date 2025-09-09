e9053d49ecfce6437ee228c62ac7ed496503b179
20251802 박민수 <neozang12345@gmail.com>
2025-09-09 16:44:29 +0900

    Hello

--- Week 2/hello.c
+++ Week 2/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello World!~");
+	int age = 20;
+	char grade = 'A';
+	char name[] = "Minsu";
+
+	printf("Hello World!\n");
+	printf("My name is %s.\n", name);
+	printf("I'm %d years old.\n", age);
+	printf("I want a %c grade.\n", grade);
+
 	return 0;
 }
---
+++ Week 2/nums.c
@@ -0,0 +1,73 @@
+// nums.c
+
+#include <stdio.h>
+#include <limits.h> //최댓값 & 최솟값 볼 때 사용
+
+int main(void) {
+
+	//char					1바이트		(문자)
+	char ch = 'A';
+	printf("[char]\n");
+	printf("Value: %c\n", ch);
+	printf("Max: %d\n", CHAR_MAX); //limit.h의 상수
+	printf("Min: %d\n", CHAR_MIN);
+
+	//signed short int		2바이트		(정수)
+	signed short int sshort = 1234;
+	printf("\n[signed short int]\n");
+	printf("Value: %d\n", sshort);
+	printf("Max: %d\n", SHRT_MAX); 
+	printf("Min: %d\n", SHRT_MIN);
+
+	//unsigned short int	2바이트		(정수)
+	unsigned short int ushort = 1234U;
+	printf("\n[unsigned short int]\n");
+	printf("Value: %d\n", ushort);
+	printf("Max: %d\n", USHRT_MAX); 
+
+	//signed int	4바이트		(정수)
+	signed int sint = 12345;
+	printf("\n[signed int]\n");
+	printf("Value: %d\n", sint);
+	printf("Max: %d\n", INT_MAX);
+	printf("Min: %d\n", INT_MIN);
+
+	//long은 다른 운영체제에서 다른 크기이다.
+	//보통 4바이트인데 Linux/Unix/Mac에서는 8바이트이다.
+	//Windows에서 long long 하면 8바이트 사용할 수 있다.
+
+	//unsigned int		4바이트		(정수)
+	unsigned int uint = 12345U;
+	printf("\n[unsigned int]\n");
+	printf("Value: %d\n", uint);
+	printf("Max: %d\n", UINT_MAX);
+
+	//signed long int		4바이트		(정수)
+	signed long int slong = 123456789L;
+	printf("\n[signed long int]\n");
+	printf("Value: %d\n", slong);
+	printf("Max: %d\n", LONG_MAX);
+	printf("Min: %d\n", LONG_MIN);
+
+	//unsigned long int		4바이트		(정수)
+	unsigned long int ulong = 123456789UL;
+	printf("\n[unsigned long int]\n");
+	printf("Value: %d\n", ulong);
+	printf("Max: %d\n", ULONG_MAX);
+
+	//float (항상 부호 있음)	4바이트		(실수)
+	float f = 123.456f;
+	printf("\n[float]\n");
+	printf("Value: %f\n", f);
+	//printf("Max: %d\n", FLT_MAX);
+	//printf("Min: %d\n", FLT_MIN);
+
+	//double (항상 부호 있음)	8바이트		(실수)
+	double d = 123456.7890123f;
+	printf("\n[double]\n");
+	printf("Value: %.3f\n", d);
+	//printf("Max: %d\n", DBL_MAX);
+	//printf("Min: %d\n", DBL_MIN);
+
+	return 0;
+}
